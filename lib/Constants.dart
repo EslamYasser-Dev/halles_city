23e20c5a64322d6093b5dfd37bb16a01293caa8a
AballahEhab <52103437+AballahEhab@users.noreply.github.com>
2020-03-15 11:16:34 +0200

    .

---
+++ lib/Constants.dart
@@ -0,0 +1,6 @@
+
+
+import 'dart:ui';
+
+const main_dark_color = Color(0xFF1A233F);
+const main_light_color = Color(0xFF009EE2);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:halles_city/screens/hall_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -7,28 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold();
-  }
-}
